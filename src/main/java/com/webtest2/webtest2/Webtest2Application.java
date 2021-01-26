220536c52ab310f63b91141cb79caa80700f8fc2
limseongkyu <96x60812@naver.com>
2021-01-27 01:32:18 +0900

    setting

---
+++ src/main/java/com/webtest2/webtest2/Webtest2Application.java
@@ -0,0 +1,13 @@
+package com.webtest2.webtest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webtest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Webtest2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/webtest2/webtest2/Webtest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webtest2.webtest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webtest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
